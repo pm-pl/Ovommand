12092fe4d09a270bf37cfa1d9e8a5435891dbdda
NTT1906 <nttispay@gmail.com>
2025-05-20 22:37:15 +0700

    CHANGE: update phpdoc, type-hinting and closure validation

--- src/galaxygames/ovommand/ClosureCommand.php
+++ src/galaxygames/ovommand/ClosureCommand.php
@@ -5,21 +5,57 @@ namespace galaxygames\ovommand;
 
 use pocketmine\command\CommandSender;
 use pocketmine\lang\Translatable;
+use pocketmine\utils\Utils;
 
+/**
+ * @phpstan-type OvommandSetupClosure \Closure(Ovommand $command) : void
+ * @phpstan-type OvommandPreRunClosure \Closure(Ovommand $command, CommandSender $sender, array $args, array $nonParsedArgs) : bool
+ * @phpstan-type OvommandRunClosure \Closure(Ovommand $command, string $label, array $args) : void
+ */
 class ClosureCommand extends Ovommand{
+	/** @phpstan-var ?OvommandSetupClosure $setupClosure */
 	private ?\Closure $setupClosure = null;
+	/** @phpstan-var ?OvommandPreRunClosure $preRunClosure */
 	private ?\Closure $preRunClosure = null;
+	/** @phpstan-var ?OvommandRunClosure $runClosure*/
 	private ?\Closure $runClosure = null;
-	public function __construct(Translatable|string $description = "", Translatable|string|null $usageMessage = null, ?string $permission = null){
+
+	/**
+	 * @phpstan-param OvommandSetupClosure $setupClosure
+	 * @phpstan-param OvommandPreRunClosure $preRunClosure
+	 * @phpstan-param OvommandRunClosure $runClosure
+	 */
+	public function __construct(
+		Translatable|string $description = "", Translatable|string|null $usageMessage = null, ?string $permission = null,
+		?\Closure $setupClosure = null, ?\Closure $preRunClosure = null, ?\Closure $runClosure = null
+	){
 		parent::__construct($description, $usageMessage, $permission);
-		Utils::registerClosure($this, "setupClosure", "setup");
+		Utils::validateCallableSignature(
+			function (Ovommand $command) : void{},
+			$setupClosure,
+		);
+		Utils::validateCallableSignature(
+			function (Ovommand $command, CommandSender $sender, array $args, array $nonParsedArgs) : bool{},
+			$preRunClosure,
+		);
+		Utils::validateCallableSignature(
+			function (Ovommand $command, string $label, array $args) : void{},
+			$runClosure,
+		);
+		$this->setupClosure = $setupClosure;
+		$this->preRunClosure = $preRunClosure;
+		$this->runClosure = $runClosure;
 	}
 
-	public function setup() : void{ $this->setupClosure?->call($this); }
+	public function setup() : void{
+		if ($this->setupClosure !== null) {
+			($this->setupClosure)($this);
+		}
+	}
 
 	public function onPreRun(CommandSender $sender, array $args, array $nonParsedArgs = []) : bool{
 		if ($this->preRunClosure !== null) {
-			return $this->preRunClosure->call($this, $sender, $args, $nonParsedArgs);
+			return ($this->preRunClosure)($this, $sender, $args, $nonParsedArgs);
 		}
 		return parent::onPreRun($sender, $args, $nonParsedArgs);
 	}
